4f67abcaddc0b0e3cc6eae14e6fac0f561f2410f
Francesco Sacchi <batt@develer.com>
2020-12-12 19:24:41 +0100

    Remove wrong option.

--- install_dep.sh
+++ install_dep.sh
@@ -4,5 +4,5 @@ set -x
 sudo apt update
 sudo apt install -y python3 python3-pip
 
-sudo pip3 install -y RPi.GPIO
-sudo pip3 install -y rpi_ws281x
+sudo pip3 install RPi.GPIO
+sudo pip3 install rpi_ws281x
